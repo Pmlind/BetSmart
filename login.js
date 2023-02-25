41a938fd6ec2a4261d5a7cd74206385f8b2ec459
m926n810 <m926n810@ku.edu>
2023-02-25 16:37:39 -0600

    cleaned up index, login

---
+++ login.js
@@ -0,0 +1,34 @@
+function signin(){
+    var username = document.getElementById("signin_username").value;
+    var password = String(CryptoJS.SHA256(document.getElementById("signin_password").value));
+    $.ajax({
+      type: "GET",
+      url: "./api/user/login.php",
+      data: { username: username, password: password }
+    })
+      .done(function( response ) {
+        // Update the page content with the response from the server
+        $("body").html(response);
+      });
+}
+
+function signup(){
+    var username = document.getElementById("signup_username").value;
+    var password = String(CryptoJS.SHA256(document.getElementById("signup_password").value));
+    var confirmm = String(CryptoJS.SHA256(document.getElementById("confirm_password").value));
+    console.log(password+" "+confirmm);
+    if(password==confirmm){
+        $.ajax({
+            type: "POST",
+            url: "./api/user/signup.php",
+            data: { username: username, password: password }
+          })
+            .done(function( response ) {
+              // Update the page content with the response from the server
+              $("body").html(response);
+            });
+        }
+    else{
+          alert("passwords are not matching");
+        }
+    }
